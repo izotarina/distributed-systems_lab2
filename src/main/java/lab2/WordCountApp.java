6a90935f45f514d0c211351444bd29856e61eaea
izotarina <irunchikt@mail.ru>
2021-10-16 00:23:39 +0300

    Scripted auto-commit on change (2021-10-16 00:23:39) by gitwatch.sh

--- src/main/java/lab2/WordCountApp.java
+++ src/main/java/lab2/WordCountApp.java
@@ -1,3 +1,5 @@
+package lab2;
+
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -9,7 +11,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class WordCountApp {
     public static void main(String[] args) throws Exception {
         if (args.length != 2) {
-            System.err.println("Usage: WordCountApp <input path> <output path>");
+            System.err.println("Usage: lab2.WordCountApp <input path> <output path>");
             System.exit(-1);
         }
         Job job = Job.getInstance();

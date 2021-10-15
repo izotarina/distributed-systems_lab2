428cadbcd257f66fe2447bf8676c41c5ad25c0f9
izotarina <irunchikt@mail.ru>
2021-10-16 01:04:46 +0300

    Scripted auto-commit on change (2021-10-16 01:04:46) by gitwatch.sh

--- src/main/java/lab2/AirportFlightsWritable.java
+++ src/main/java/lab2/AirportFlightsWritable.java
@@ -1,4 +1,26 @@
 package lab2;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class AirportFlightsWritable {
+    private int airportId;
+    private String airportName;
+
+    public void write(DataOutput out) throws IOException {
+        out.writeInt(airportId);
+        out.writeChars(airportName);
+    }
+
+    public void readFields(DataInput in) throws IOException {
+        airportId = in.readInt();
+        airportName = in.readLine();
+    }
+
+    public static AirportFlightsWritable read(DataInput in) throws IOException {
+        AirportFlightsWritable w = new AirportFlightsWritable();
+        w.readFields(in);
+        return w;
+    }
 }

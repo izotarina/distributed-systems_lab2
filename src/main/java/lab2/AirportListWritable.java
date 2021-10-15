9f160913b0d30bde776ab4a05edc79fade642cc8
izotarina <irunchikt@mail.ru>
2021-10-16 01:00:24 +0300

    Scripted auto-commit on change (2021-10-16 01:00:24) by gitwatch.sh

--- src/main/java/lab2/AirportListWritable.java
+++ src/main/java/lab2/AirportListWritable.java
@@ -1,4 +1,29 @@
 package lab2;
 
-public class AirportListWritable {
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class AirportListWritable implements Writable {
+    // Some data
+    private int counter;
+    private long timestamp;
+
+    public void write(DataOutput out) throws IOException {
+        out.writeInt(counter);
+        out.writeLong(timestamp);
+    }
+
+    public void readFields(DataInput in) throws IOException {
+        counter = in.readInt();
+        timestamp = in.readLong();
+    }
+
+    public static AirportListWritable read(DataInput in) throws IOException {
+        AirportListWritable w = new AirportListWritable();
+        w.readFields(in);
+        return w;
+    }
 }

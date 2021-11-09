5f6e061f082df9f576bbd9db26522573a412512f
izotarina <irunchikt@mail.ru>
2021-11-09 21:52:40 +0300

    Scripted auto-commit on change (2021-11-09 21:52:40) by gitwatch.sh

--- src/main/java/lab2/AirportDelayApp.java
+++ src/main/java/lab2/AirportDelayApp.java
@@ -4,7 +4,6 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -12,17 +11,17 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class AirportDelayApp {
     public static void main(String[] args) throws Exception {
-        if (args.length != 2) {
-            System.err.println("Usage: lab2.AirportDelayApp <input path> <output path>");
+        if (args.length != 3) {
+            System.err.println("Usage: lab2.AirportDelayApp <input1 path> <input2 path> <output path>");
             System.exit(-1);
         }
+
         Job job = Job.getInstance();
         job.setJarByClass(AirportDelayApp.class);
         job.setJobName("Airport delay");
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, );
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        job.setMapperClass(WordMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlightsMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
         job.setReducerClass(WordReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);

688ac78d3ed110898c1254db9ed1e881f11b1d68
izotarina <irunchikt@mail.ru>
2021-11-09 21:58:21 +0300

    Scripted auto-commit on change (2021-11-09 21:58:21) by gitwatch.sh

--- src/main/java/lab2/AirportFlightsWritable.java
+++ src/main/java/lab2/AirportFlightsWritable.java
@@ -1,13 +1,21 @@
 package lab2;
 
+import org.apache.hadoop.io.Writable;
+
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
-public class AirportFlightsWritable {
+public class AirportFlightsWritable implements Writable {
     private int airportId;
     private boolean isCanceled;
     private int delayTime;
+
+    public AirportFlightsWritable(int airportId, boolean isCanceled, int delayTime) {
+        this.airportId = airportId;
+        this.isCanceled = isCanceled;
+        this.delayTime = delayTime;
+    }
 
     public void write(DataOutput out) throws IOException {
         out.writeInt(airportId);
--- src/main/java/lab2/FlightsMapper.java
+++ src/main/java/lab2/FlightsMapper.java
@@ -1,20 +1,22 @@
 package lab2;
 
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class FlightsMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+public class FlightsMapper extends Mapper<LongWritable, Text, KeyWritableComparable, String> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String inputLine = value.toString();
-        String[] words = inputLine.split("[\\p{Punct}\\p{Space}—]");
-        for (String word: words) {
-            word = word.toLowerCase();
-            context.write(new Text(word),new IntWritable(1));
+        String[] columns = inputLine.split(",");
+
+        if (key.get() != 0) {
+            context.write(
+                    new KeyWritableComparable(Integer.parseInt(columns[0]), 0),
+                    new AirportListWritable(Integer.parseInt(columns[0]), columns[1]).getAirportName()
+            );
         }
     }
 }

d10fabd8cf9f0c3d46e706d34f0de421c699130b
izotarina <irunchikt@mail.ru>
2021-11-11 08:27:35 +0300

    Scripted auto-commit on change (2021-11-11 08:27:35) by gitwatch.sh

--- src/main/java/lab2/KeyPartitioner.java
+++ src/main/java/lab2/KeyPartitioner.java
@@ -4,5 +4,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 
 public class KeyPartitioner extends Partitioner<KeyWritableComparable, Text> {
-    
+    @Override
+    getPartition()
 }

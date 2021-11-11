9ef95368f40a44878cf6e5fafe4df15a869513e8
izotarina <irunchikt@mail.ru>
2021-11-11 08:42:54 +0300

    Scripted auto-commit on change (2021-11-11 08:42:54) by gitwatch.sh

---
+++ src/main/java/lab2/AirportReducer.java
@@ -0,0 +1,22 @@
+package lab2;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class AirportReducer extends Reducer<Text, IntWritable, Text, LongWritable> {
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        long count = 0;
+        Iterator<IntWritable> iterator = values.iterator();
+        while (iterator.hasNext()) {
+            iterator.next();
+            count++;
+        }
+        context.write(key, new LongWritable(count));
+    }
+}

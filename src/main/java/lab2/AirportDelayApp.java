97bc6911b74acb4239e1e08c356fb4cb6b80bf29
izotarina <irunchikt@mail.ru>
2021-11-11 08:30:42 +0300

    Scripted auto-commit on change (2021-11-11 08:30:42) by gitwatch.sh

--- src/main/java/lab2/AirportDelayApp.java
+++ src/main/java/lab2/AirportDelayApp.java
@@ -22,6 +22,7 @@ public class AirportDelayApp {
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlightsMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setPartitionerClass(KeyPartitioner.class);
         job.setReducerClass(WordReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);

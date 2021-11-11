590813beaf892eaeaf5e62f9725c305663cbba79
izotarina <irunchikt@mail.ru>
2021-11-11 08:34:38 +0300

    Scripted auto-commit on change (2021-11-11 08:34:38) by gitwatch.sh

--- src/main/java/lab2/AirportGroupingComparator.java
+++ src/main/java/lab2/AirportGroupingComparator.java
@@ -1,4 +1,18 @@
 package lab2;
 
-public class AirportGroupingComarator {
+import org.apache.hadoop.io.WritableComparable;
+
+public class AirportGroupingComparator {
+    public AirportGroupingComparator(){
+        super(KeyWritableComparable.class);
+    }
+
+    @Override
+    public int compare(WritableComparable o, WritableComparable o2){
+        System.out.println("in compare");
+        Movie m = (Movie)o;
+        Movie m2 = (Movie)o2;
+        System.out.println(m.compareTo(m2));
+        return m.movieId.compareTo(m2.movieId);
+    }
 }

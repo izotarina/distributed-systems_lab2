f09178c55d7f05ffc470e5db3c1b708c299ee275
izotarina <irunchikt@mail.ru>
2021-11-13 16:11:33 +0300

    Scripted auto-commit on change (2021-11-13 16:11:33) by gitwatch.sh

--- src/main/java/lab2/AirportDelayApp.java
+++ src/main/java/lab2/AirportDelayApp.java
@@ -1,7 +1,6 @@
 package lab2;
 
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;

e7c08b0c6610bd007663de09992c7a3d32761d31
izotarina <irunchikt@mail.ru>
2021-11-22 23:39:54 +0300

    Scripted auto-commit on change (2021-11-22 23:39:54) by gitwatch.sh

--- src/main/java/lab2/AirportGroupingComparator.java
+++ src/main/java/lab2/AirportGroupingComparator.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.io.WritableComparator;
 
 public class AirportGroupingComparator extends WritableComparator {
     public AirportGroupingComparator(){
-        super(KeyWritableComparable.class);
+        super(KeyWritableComparable.class, true);
     }
 
     @Override

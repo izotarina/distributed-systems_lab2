2829e8fd5e51d9b3b28a5caf92744dc91f526960
izotarina <irunchikt@mail.ru>
2021-12-04 12:42:26 +0300

    Scripted auto-commit on change (2021-12-04 12:42:26) by gitwatch.sh

--- src/main/java/lab2/AirportMapper.java
+++ src/main/java/lab2/AirportMapper.java
@@ -21,7 +21,7 @@ public class AirportMapper extends Mapper<LongWritable, Text, KeyWritableCompara
 
             context.write(
                     new KeyWritableComparable(airportCode, 0),
-                    new Text(airportName)
+                    new Text(new AirportListWritable(airportCode, airportName).getAirportName())
             );
         }
     }

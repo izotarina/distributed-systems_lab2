060fa3cd8449e73278295524b0507a36ea903b38
izotarina <irunchikt@mail.ru>
2021-12-04 12:06:07 +0300

    Scripted auto-commit on change (2021-12-04 12:06:07) by gitwatch.sh

--- src/main/java/lab2/FlightsMapper.java
+++ src/main/java/lab2/FlightsMapper.java
@@ -8,19 +8,21 @@ import java.io.IOException;
 
 public class FlightsMapper extends Mapper<LongWritable, Text, KeyWritableComparable, Text> {
     private final static int AIRPORT_COLUMN_INDEX = 14;
-    private
+    private final static int DELAY_COLUMN_INDEX = 18;
+    private final static int IS_CANCELLED_COLUMN_INDEX = 19;
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String inputLine = value.toString();
         String[] columns = inputLine.split(",");
 
         if (key.get() != 0) {
-            float delayTime = new AirportFlightsWritable(Integer.parseInt(columns[AIRPORT_COLUMN_INDEX]), columns[19].length() > 0 ? Float.parseFloat(columns[19]) : 0,
-                    columns[18].length() > 0 ? Float.parseFloat(columns[18]) : 0).getDelayTime();
+            float delayTime = new AirportFlightsWritable(Integer.parseInt(columns[AIRPORT_COLUMN_INDEX]),
+                    columns[IS_CANCELLED_COLUMN_INDEX].length() > 0 ? Float.parseFloat(columns[IS_CANCELLED_COLUMN_INDEX]) : 0,
+                    columns[DELAY_COLUMN_INDEX].length() > 0 ? Float.parseFloat(columns[DELAY_COLUMN_INDEX]) : 0).getDelayTime();
 
             if (delayTime > 0) {
                 context.write(
-                        new KeyWritableComparable(Integer.parseInt(columns[14]), 1),
+                        new KeyWritableComparable(Integer.parseInt(columns[AIRPORT_COLUMN_INDEX]), 1),
                         new Text(String.valueOf(delayTime))
                 );
             }

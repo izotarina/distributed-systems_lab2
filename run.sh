a6c992342dd03e664a014bc39682ff0bd6b1f4b5
izotarina <irunchikt@mail.ru>
2021-12-04 13:16:01 +0300

    Scripted auto-commit on change (2021-12-04 13:16:01) by gitwatch.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 mvn package
 hadoop fs -rm -f -r output
 export HADOOP_CLASSPATH=./target/hadoop-examples-1.0-SNAPSHOT.jar
-hadoop App L_AIRPORT_ID.csv 664600583_T_ONTIME_sample.csv output
+hadoop lab2.AirportDelayApp L_AIRPORT_ID.csv 664600583_T_ONTIME_sample.csv output
 rm -rf output
 hadoop fs -copyToLocal output
